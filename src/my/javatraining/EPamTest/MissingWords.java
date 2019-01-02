01c99ec0f99f734af17edfa7482f6b33cf35a98a
mehdi <mehdi@mobilityonegroup.com>
2019-01-03 01:50:35 +0800

    fixed missingwords alghorithm to pass more test cases

--- src/my/javatraining/EPamTest/MissingWords.java
+++ src/my/javatraining/EPamTest/MissingWords.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 public class MissingWords {
     public static void main(String[] args){
-        String t = "I am using hackerrank to improve programming";
-        String s = "am hackerrank to improve";
-        List<String> str = missingwords(t,s);
+
+        String s = " Programmer Programmer Programmer ";
+        String t = " Programmer Programmer";
+        List<String> str = missingwords2(s,t);
         for(int i=0;i<str.size();i++)
             System.out.println(str.get(i));
+
     }
 
-    public static List<String> missingwords(String s, String t){
+    public static List<String> missingwords2(String s, String t){
 
         String[] a = s.split(" ");
         String[] b = t.split(" ");
@@ -21,8 +23,11 @@ public class MissingWords {
         for(int i=0;i<a.length;i++){
             boolean isMatched=false;
             for(int j=0;j<b.length;j++){
-                if(a[i].equals(b[j]))
-                    isMatched=true;
+                if(a[i].equals(b[j])) {
+                    isMatched = true;
+                    b[j]="";
+                    break;
+                }
             }
             if(!isMatched){
                 missing.add(a[i]);
